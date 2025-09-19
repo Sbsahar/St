bc180fe7b82ddd6d83f3ce63b770b0e8cb0db656
Sbsahar <samo3696385@gmail.com>
2025-09-19 19:50:49 +0300

    Update stt.py

--- stt.py
+++ stt.py
@@ -46,8 +46,8 @@ active_games = {}    # {game_id: {'mode': 'pvp' or 'bot', 'players': [p1, p2 or 
 # رموز الإيموجي
 MOVE_DOT = '🔵'
 CAPTURE_DOT = '🔴'
-SQUARE_LIGHT = ' '
-SQUARE_DARK = ' '
+SQUARE_LIGHT = '.'
+SQUARE_DARK = '.'
 
 PIECE_TO_EMOJI = {
     'P': '♙', 'R': '♖', 'N': '♘', 'B': '♗', 'Q': '♕', 'K': '♔',
